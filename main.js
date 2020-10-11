2e4757020236ba674e83618d1a73264613551890
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-10-11 17:29:24 -0400

    Create click event listeners (ALL BOXES)

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("box-1").addEventListener("click", function() {
+    document.getElementById("box-1").innerHTML = "Oooh - so close, but no cigar";
+  });
+
+  document.getElementById("box-2").addEventListener("click", function() {
+    document.getElementById("box-2").innerHTML = "DING DING DING - We have a winner";
+  });
+
+  document.getElementById("box-3").addEventListener("click", function() {
+    document.getElementById("box-3").innerHTML = " Oops, butter luck next time";
+  });
